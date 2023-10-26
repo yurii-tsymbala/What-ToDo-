f25e084ff9b03f4885cdd54e7845358b0fcc7ecc
Yurii <zumbala2013@gmail.com>
2023-10-26 19:47:33 +0300

    Configure CRUD in TaskService

---
+++ src/TaskService.js
@@ -0,0 +1,42 @@
+class Task {
+  constructor(name) {
+    this.name = name;
+    this.id = require("crypto").randomUUID();
+  }
+}
+
+class TaskService {
+  constructor() {
+    this.tasks = this.generateDefaultTasks();
+  }
+
+  createTask(name) {
+    let task = new Task(name);
+    this.tasks.push(task);
+  }
+
+  updateTask(task) {
+    let updatedTask = this.tasks.find((el) => el.id === task.id);
+    updatedTask.name = task.name;
+  }
+
+  deleteTask(task) {
+    this.tasks = this.tasks.filter((el) => el.id !== task.id);
+  }
+
+  allTasks() {
+    return this.tasks;
+  }
+
+  generateDefaultTasks() {
+    let task1 = new Task("Buy pizza");
+    let task2 = new Task("Buy bread");
+    let task3 = new Task("Buy milk");
+    return [task1, task2, task3];
+  }
+}
+
+module.exports = {
+  TaskService, 
+  Task
+}
--- src/main.js
+++ src/main.js
@@ -1,12 +1,19 @@
+const { TaskService, Task } = require("./TaskService");
 const express = require("express");
 const path = require("path");
-
 const app = express();
 const port = process.env.PORT || 3000;
-
 app.get("/", function (req, res) {
   res.sendFile(path.join(__dirname, "/index.html"));
 });
-
+app.get("/tasks.js", function (req, res) {
+  res.sendFile(path.join(__dirname, "/tasks.js"));
+});
 app.listen(port);
 console.log("Server started at http://localhost:" + port);
+
+let task = new Task("Wash the dishes");
+console.log(task);
+
+let taskService = new TaskService();
+console.log(taskService.allTasks());
