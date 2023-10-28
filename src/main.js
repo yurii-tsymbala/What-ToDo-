1b92957b02fb77f2eee55a138412bee621a01d5d
Yurii <zumbala2013@gmail.com>
2023-10-28 18:00:36 +0300

    Add test api method for fetching tasks

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
+const cors = require("cors");
 const express = require("express");
 const path = require("path");
 
 const app = express();
 const port = process.env.PORT || 3001;
 
-// app.get("/", function (req, res) {
-//   res.sendFile(path.join(__dirname, "/index.html"));
-// });
+app.use(cors());
+
+app.get("/tasks", (request, response) => {
+  response.json([
+    { id: 546, name: "Buy pizza" },
+    { id: 894, name: "Sold car" },
+    { id: 326, name: "Rent house" },
+  ]);
+});
 
 app.listen(port);
-console.log("Server started at http://localhost:" + port);
-
-
+console.log("Backend Server started at http://localhost:" + port);
