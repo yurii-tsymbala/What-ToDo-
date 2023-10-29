c9f61cde315ebfa625d7dca89e5fbd798aa3af11
Yurii <zumbala2013@gmail.com>
2023-10-30 00:25:15 +0200

    Refactor CRUD services with response(success,error)

--- js/TaskService.js
+++ js/TaskService.js
@@ -2,38 +2,76 @@ import { v4 as uuid } from "uuid";
 
 export class Task {
   constructor(name) {
-    this.name = name;
     this.id = uuid();
+    this.name = name;
   }
 }
 
-export class TaskService {
-  constructor() {
-    this.tasks = this.generateDefaultTasks();
-  }
+export const TaskService = new (class {
+  constructor() {}
 
   createTask(name) {
-    let task = new Task(name);
-    this.tasks.unshift(task);
+    return new Promise(function (myResolve, myReject) {
+      let task = new Task(name);
+      let xhr = new XMLHttpRequest();
+      xhr.open("POST", "http://localhost:3001/addtask", true);
+      xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+      xhr.onload = function () {
+        if (xhr.status == 201) {
+          myResolve();
+        } else {
+          myReject("Failed to create task");
+        }
+      };
+      xhr.send(JSON.stringify(task));
+    });
   }
 
   updateTask(task) {
-    let updatedTask = this.tasks.find((el) => el.id === task.id);
-    updatedTask.name = task.name;
+    return new Promise(function (myResolve, myReject) {
+      let xhr = new XMLHttpRequest();
+      xhr.open("PUT", "http://localhost:3001/" + task.id, true);
+      xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+      xhr.onload = function () {
+        if (xhr.status == 204) {
+          myResolve();
+        } else {
+          myReject("Failed to update task");
+        }
+      };
+      xhr.send(JSON.stringify(task));
+    });
   }
 
   deleteTask(task) {
-    this.tasks = this.tasks.filter((el) => el.id !== task.id);
+    return new Promise(function (myResolve, myReject) {
+      let xhr = new XMLHttpRequest();
+      xhr.open("DELETE", "http://localhost:3001/" + task.id, true);
+      xhr.setRequestHeader("Content-type", "application/json; charset=utf-8");
+      xhr.onload = function () {
+        if (xhr.status == 200) {
+          myResolve();
+        } else {
+          myReject("Failed to delete task");
+        }
+      };
+      xhr.send(JSON.stringify(task));
+    });
   }
 
   allTasks() {
-    return this.tasks;
+    return new Promise(function (myResolve, myReject) {
+      let xhr = new XMLHttpRequest();
+      xhr.open("GET", "http://localhost:3001/tasks");
+      xhr.onload = function () {
+        if (xhr.status == 200) {
+          const tasks = JSON.parse(xhr.responseText);
+          myResolve(tasks);
+        } else {
+          myReject("Tasks not found");
+        }
+      };
+      xhr.send();
+    });
   }
-
-  generateDefaultTasks() {
-    let task1 = new Task("Buy pizza");
-    let task2 = new Task("Buy bread");
-    let task3 = new Task("Buy milk");
-    return [task1, task2, task3];
-  }
-}
+})();
