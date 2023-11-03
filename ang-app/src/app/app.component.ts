fb57a7539b0e7aa152bec723c617d39d72eb84b6
Yurii <zumbala2013@gmail.com>
2023-11-03 02:30:54 +0200

    Configure AppComponent with CRUD updates

--- ang-app/src/app/app.component.ts
+++ ang-app/src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { TaskService } from './services/task.service';
+import { Task } from './Task';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'ang-app';
+export class AppComponent implements OnInit {
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit(): void {
+    this.loadTasks();
+  }
+
+  async loadTasks() {
+    this.tasks = await this.taskService.allTasks();
+  }
+
+  createTask() {
+    console.log('Task created');
+  }
 }
