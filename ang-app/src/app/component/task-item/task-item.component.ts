465fb5514f66357dca7cceb4be67fcb8b608fe43
Yurii <zumbala2013@gmail.com>
2023-11-04 16:41:53 +0200

    Add update task feature

--- ang-app/src/app/component/task-item/task-item.component.ts
+++ ang-app/src/app/component/task-item/task-item.component.ts
@@ -1,29 +1,34 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
 import { Task } from 'src/app/Task';
 
 @Component({
   selector: 'task-item',
-  templateUrl: './task-item.component.html'
+  templateUrl: './task-item.component.html',
 })
 export class TaskItemComponent implements OnInit {
   @Input() task!: Task;
-  @Output() btnClickSave = new EventEmitter();
+  @Output() btnClickUpdate = new EventEmitter();
   @Output() btnClickDelete = new EventEmitter();
-  isEditing = false; 
+  isEditing = false;
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  onUpdateClicked(taskUpdateName: HTMLInputElement) {
+    if (taskUpdateName.value.length < 5) {
+      taskUpdateName.value = '';
+      taskUpdateName.placeholder = 'Too small )=';
+      return;
+    }
+    this.task.name = taskUpdateName.value;
+    this.btnClickUpdate.emit(this.task);
   }
 
-  onSaveClicked() {
-    this.btnClickSave.emit();
+  onEditClicked(taskUpdateName: HTMLInputElement) {
+    taskUpdateName.value = this.task.name;
+    this.isEditing = true;
   }
 
-  onEditClicked()  {
-   this.isEditing = true;
-    
-  }
-
-  onDeleteClicked()  {
+  onDeleteClicked() {
     this.btnClickDelete.emit(this.task);
   }
 }
