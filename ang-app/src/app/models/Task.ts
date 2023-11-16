8a9e04f4d13ce4ffe4bda3031b53b4f57f6e4fa6
Yurii <zumbala2013@gmail.com>
2023-11-16 20:15:13 +0200

    Refactor Task to TypeScript style

--- ang-app/src/app/models/Task.ts
+++ ang-app/src/app/models/Task.ts
@@ -1,10 +1,7 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export class Task {
-  id: string;
-  name: string;
-  constructor(name: string) {
-    this.id = uuidv4();
+  constructor(public name: string, public id: string = uuidv4()) {
     this.name = name;
   }
 }
