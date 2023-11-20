dcedec10677b36545d23fc5c71fd27b8a3e14687
Yurii <zumbala2013@gmail.com>
2023-11-20 20:48:13 +0200

    Fix TaskService

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,70 +5,67 @@ import { TaskService, Task } from "./TaskService";
 import TaskInput from "./TaskInput";
 
 export default class App extends React.Component {
-  constructor(props) {
-    super(props);
+    constructor(props) {
+        super(props);
 
-    this.state = {
-      tasks: [],
-    };
-  }
-  componentDidMount() {
-    this.loadTasks();
-  }
+        this.state = { tasks: [] };
+    }
+    componentDidMount() {
+        this.loadTasks();
+    }
 
-  async loadTasks() {
-    const tasks = await TaskService.allTasks();
-    this.setState({ tasks });
-  }
+    async loadTasks() {
+        const tasks = await TaskService.allTasks();
+        this.setState({ tasks });
+    }
 
-  async createTask(taskName) {
-    await TaskService.createTask(taskName);
-    await this.loadTasks();
-  }
+    async createTask(taskName) {
+        await TaskService.createTask(taskName);
+        await this.loadTasks();
+    }
 
-  async saveTask(taskId, taskName) {
-    await TaskService.updateTask({
-      id: taskId,
-      name: taskName,
-    });
-    await this.loadTasks();
-  }
+    async saveTask(taskId, taskName) {
+        await TaskService.updateTask({
+            id: taskId,
+            name: taskName,
+        });
+        await this.loadTasks();
+    }
 
-  async deleteTask(taskId) {
-    await TaskService.deleteTask(taskId);
-    await this.loadTasks();
-  }
+    async deleteTask(taskId) {
+        await TaskService.deleteTask(taskId);
+        await this.loadTasks();
+    }
 
-  renderTaskInput() {
-    return (<TaskInput
-    key={434}
-    onSave={(newTaskName) => this.createTask(newTaskName)} 
-    />);
-  }
+    renderTaskInput() {
+        return (
+            <TaskInput onSave={(newTaskName) => this.createTask(newTaskName)} />
+        );
+    }
 
-  renderTaskItems() {
-    return this.state.tasks.map((task) => {
-      return (
-        <TaskItem
-          key={task.id}
-          id={task.id}
-          name={task.name}
-          onDelete={() => this.deleteTask(task.id)}
-          onSave={(newName) => this.saveTask(task.id, newName)}
-        />
-      );
-    });
-  }
+    renderTaskItems() {
+        return this.state.tasks.map((task) => {
+            return (
+                <TaskItem
+                    key={task.id}
+                    id={task.id}
+                    name={task.name}
+                    onDelete={() => this.deleteTask(task.id)}
+                    onSave={(newName) => this.saveTask(task.id, newName)}
+                />
+            );
+        });
+    }
 
-  render() {
-    return (
-      <div className="App">
-        <div id="background"></div>
-        <div id="center" className="divWithBorder">
-          {this.renderTaskInput()}
-          <div id="list">{this.renderTaskItems()}</div>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <div id="background"></div>
+                <div id="center" className="divWithBorder">
+                    {this.renderTaskInput()}
+                    <div id="list">{this.renderTaskItems()}</div>
+                </div>
+            </div>
+        );
+    }
 }
